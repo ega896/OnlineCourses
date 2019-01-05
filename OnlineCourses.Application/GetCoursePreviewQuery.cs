982bcbd5d113088620963a6e209de462378e0dc9
ekenda <egor.kenney@gmail.com>
2019-01-06 00:42:39 +0300

    Initial commit

---
+++ OnlineCourses.Application/GetCoursePreviewQuery.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace OnlineCourses.Application
+{
+    public class GetCoursePreviewQuery : IRequest<List<CoursePreviewDto>>
+    {
+        public int Id { get; set; }
+    }
+
+    public class CoursePreviewDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ OnlineCourses.WebAPI/Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using System.Threading.Tasks;
+using OnlineCourses.Application;
+
+namespace OnlineCourses.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : BaseController
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetCategoryPreview([FromQuery] GetCoursePreviewQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
